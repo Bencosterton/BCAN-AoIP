f90a9b8462ca478b17eeacaa4af1ed7db921453a
Ben Costerton <bencosterton@gmail.com>
2023-02-10 01:56:46 +0000

    Update transmitter.py

--- transmitter.py
+++ transmitter.py
@@ -5,36 +5,56 @@ import pyaudio
 from threading import Thread
 import time
 import sys
-import ntplib
-from datetime import datetime, timezone
+try:
+    import win_ntp
+except ImportError:
+    import unix_ntp
+
+#Windows ntp
+try:
+    win_ntp.gettime_ntp()
+except:
+    pass
 
 def wait(amount):
     time.sleep(amount)
     print()
+    
+def progressbar(it, prefix="", size=60, out=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        print("{}[{}{}] {}/{}".format(prefix, "#"*x, "."*(size-x), j, count), 
+                end='\r', file=out, flush=True)
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    print(flush=True, file=out, end="\r")
 
 NAME = socket.gethostname()
 T_IP = socket.gethostbyname(NAME)
 
 #Welcome message
 print("""\
- _  _  _       _                                        ______   ______        ______  
-| || || |     | |                          _           (____  \ / _____)  /\  |  ___ \ 
-| || || | ____| | ____ ___  ____   ____   | |_  ___     ____)  ) /       /  \ | |   | |
-| ||_|| |/ _  ) |/ ___) _ \|    \ / _  )  |  _)/ _ \   |  __  (| |      / /\ \| |   | |
-| |___| ( (/ /| ( (__| |_| | | | ( (/ /   | |_| |_| |  | |__)  ) \_____| |__| | |   | |
- \______|\____)_|\____)___/|_|_|_|\____)   \___)___/   |______/ \______)______|_|   |_| 
-  TRANSMITTER -""", NAME, T_IP,)
+ ______   ______        ______     _______                             _                     
+(____  \ / _____)  /\  |  ___ \   (_______)                           (_)_   _               
+ ____)  ) /       /  \ | |   | |   _        ____ ____ ____   ___ ____  _| |_| |_  ____  ____ 
+|  __  (| |      / /\ \| |   | |  | |      / ___) _  |  _ \ /___)    \| |  _)  _)/ _  )/ ___)
+| |__)  ) \_____| |__| | |   | |  | |_____| |  ( ( | | | | |___ | | | | | |_| |_( (/ /| |    
+|______/ \______)______|_|   |_|   \______)_|   \_||_|_| |_(___/|_|_|_|_|\___)___)____)_|    
+""", NAME, T_IP,)
 
 wait(1)
 
 #Input Audio
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
+Audio = pyaudio.PyAudio()
+info = Audio.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 
 for i in range(0, numdevices):
-    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    if (Audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", Audio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 Au_Input = input("Which input are we using?: ")
 Input = int(Au_Input)
@@ -42,17 +62,8 @@ Input = int(Au_Input)
 #Input Network
 IP_input = input("Give me an IP, dude: ")
 IP = str(IP_input)
-UDP = 6980
+UDP = 14505
 frames = []
-
-#NTP Request
-try:
-    t = ntplib.NTPClient()
-    response = t.request('time.google.com', version=3)
-    response.offset
-    #print (datetime.fromtimestamp(response.tx_time, timezone.utc))
-except:
-    pass
 
 
 #Data send
@@ -62,13 +73,10 @@ def udpStream():
     while True:
         if len(frames) > 0:
             udp.sendto(frames.pop(0), (IP, UDP))
-            print ("Sending audio to",IP,"at",UDP)
-            sys.stdout.flush()
-            print ("Sending audio to",IP,"at",UDP," .")
-            sys.stdout.flush()
-            print ("Sending audio to",IP,"at",UDP," ..")
-            sys.stdout.flush()
+            for i in progressbar(range(10), f"Sending audio to {IP} at {UDP} ", 20):
+                time.sleep(0)
     udp.close()
+
 
 def record(stream, CHUNK):
     while True:
